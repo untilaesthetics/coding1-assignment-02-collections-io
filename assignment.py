24c426f4a5340933564b18e153b4f54f8dcaac49
Aman Antil <amanantil860707@gmail.com>
2026-09-23 17:20:21 +0200

    Have put the comments for each line explained

--- assignment.py
+++ assignment.py
@@ -1,6 +1,6 @@
-drink = "tea"
-sales = {"tea": 3, "coffee": 5}
-sold = sales[drink]
-sold = sold + 1
-print(drink)
-print(sold)
+drink = "tea" # defined the drink variable
+sales = {"tea": 3, "coffee": 5} # defined the sales variable, but the coffee variable is not defined.
+sold = sales[drink] # defined the sold variable
+sold = sold + 1 # defined the sold variable to add 1 to the sold variable which was defined in the line 3
+print(drink) #Now its printing the drink variable which is tea
+print(sold) #and the sold variable which is 4 because we added 1 to the sold variable which was 3 in the line 3
